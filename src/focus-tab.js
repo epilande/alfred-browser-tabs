f043648b5b982507b1d26d6c2b29b4c81d951f4a
Aly Thobani <athobani9@gmail.com>
2024-05-12 11:55:23 -0700

    refactor: extract out fn and add docstrings

--- src/focus-tab.js
+++ src/focus-tab.js
@@ -3,21 +3,36 @@
 function run(args) {
   ObjC.import("stdlib");
 
-  const browserName = $.getenv("browser");
   const { windowIndex, tabIndex } = getWindowAndTabIndex(args);
-
-  const browser = Application(browserName);
-  // Defining these at the same time here, before their ordering gets affected by `activateTab`
-  const browserWindow = browser.windows[windowIndex];
-  const systemWindow = getSystemWindow(browserName, windowIndex);
+  const { browser, browserWindow, systemWindow } =
+    getBrowserAndWindows(windowIndex);
 
   activateTab(browser, browserWindow, systemWindow, tabIndex);
 }
 
+/**
+ * Parses the window index and tab index (both 0-indexed) from the query string.
+ */
 function getWindowAndTabIndex(args) {
   const query = args[0];
   const [windowIndex, tabIndex] = query.split(",").map((x) => parseInt(x));
+  console.log(`windowIndex: ${windowIndex}, tabIndex: ${tabIndex}`);
   return { windowIndex, tabIndex };
+}
+
+/**
+ * Returns the browser application object, along with the specific browser window and the system
+ * window object for the given window index.
+ *
+ * We define `browserWindow` and `systemWindow` here at the same time, before the window ordering
+ * gets affected by `activateTab` and `windowIndex` is no longer accurate.
+ */
+function getBrowserAndWindows(windowIndex) {
+  const browserName = $.getenv("browser");
+  const browser = Application(browserName);
+  const browserWindow = browser.windows[windowIndex];
+  const systemWindow = getSystemWindow(browserName, windowIndex);
+  return { browser, browserWindow, systemWindow };
 }
 
 function getSystemWindow(browserName, windowIndex) {
@@ -25,6 +40,10 @@ function getSystemWindow(browserName, windowIndex) {
   return browserProcess.windows[windowIndex];
 }
 
+/**
+ * Activates the tab at the given index in the browser window, focuses the window, and activates the
+ * browser application.
+ */
 function activateTab(browser, browserWindow, systemWindow, tabIndex) {
   browserWindow.activeTabIndex = tabIndex + 1;
   systemWindow.actions["AXRaise"].perform();
