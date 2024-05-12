1f0afaf740a7af55ac78fe87b3dd59060a4c3594
Aly Thobani <athobani9@gmail.com>
2024-05-12 11:25:22 -0700

    fix: use AXRaise action to focus window properly

--- src/focus-tab.js
+++ src/focus-tab.js
@@ -2,15 +2,31 @@
 
 function run(args) {
   ObjC.import("stdlib");
-  let browser = $.getenv("browser");
-  let chrome = Application(browser);
-  let query = args[0];
-  let [arg1, arg2] = query.split(",");
-  let windowIndex = parseInt(arg1);
-  let tabIndex = parseInt(arg2);
 
-  chrome.windows[windowIndex].visible = true;
-  chrome.windows[windowIndex].activeTabIndex = tabIndex + 1;
-  chrome.windows[windowIndex].index = 1;
-  chrome.activate();
+  const browserName = $.getenv("browser");
+  const { windowIndex, tabIndex } = getWindowAndTabIndex(args);
+
+  const browser = Application(browserName);
+  // Defining these at the same time here, before their ordering gets affected by `activateTab`
+  const browserWindow = browser.windows[windowIndex];
+  const systemWindow = getSystemWindow(browserName, windowIndex);
+
+  activateTab(browser, browserWindow, systemWindow, tabIndex);
+}
+
+function getWindowAndTabIndex(args) {
+  const query = args[0];
+  const [windowIndex, tabIndex] = query.split(",").map((x) => parseInt(x));
+  return { windowIndex, tabIndex };
+}
+
+function getSystemWindow(browserName, windowIndex) {
+  const browserProcess = Application("System Events").processes[browserName];
+  return browserProcess.windows[windowIndex];
+}
+
+function activateTab(browser, browserWindow, systemWindow, tabIndex) {
+  browserWindow.activeTabIndex = tabIndex + 1;
+  systemWindow.actions["AXRaise"].perform();
+  browser.activate();
 }
