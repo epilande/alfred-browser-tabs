f1b600473418f6bb3a6caee4970306eb85f0ea3f
spamwax <1251233+spamwax@users.noreply.github.com>
2024-06-11 04:02:11 -0400

    feat: Add support for Arc browser. (#50)
    * Add support for Arc browser.
    
    - Arc has "Spaces" concept which encapsulates tabs into groups, and consequently
      it adds new properties and fuactions to its AppleScript Dictionary. This PR
      adds two new JS scripts to deal with Arc's variations compared to other
      Chrome-based browsers.
    
    * fix: small typo
    
    * Merge upstream chanegs done to info.plist

---
+++ src/focus-tab-arc.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env osascript -l JavaScript
+
+function run(args) {
+  ObjC.import("stdlib");
+  let browser = $.getenv("browser");
+  let chrome = Application(browser);
+  let query = args[0];
+  let [arg1, arg2, arg3] = query.split(",");
+
+  let windowIndex = parseInt(arg1);
+  let spaceIndex = parseInt(arg2);
+  let tabIndex = parseInt(arg3);
+
+  chrome.windows[windowIndex].spaces[spaceIndex].focus();
+  chrome.windows[windowIndex].spaces[spaceIndex].tabs[tabIndex].select();
+  chrome.activate()
+}
